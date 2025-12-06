4834cbd0ff9929b6f8ba4d41710028aee083870a
okoeee <yk27.puxcm@gmail.com>
2025-12-06 11:33:23 +0900

    remove unused test

--- src/test/scala/extensibleeffects/ExtensibleEffectsTest.scala
+++ src/test/scala/extensibleeffects/ExtensibleEffectsTest.scala
@@ -2,11 +2,4 @@ package extensibleeffects
 
 import org.scalatest.freespec.AnyFreeSpec
 
-class ExtensibleEffectsTest extends AnyFreeSpec {
-
-  "テスト" in {
-    val mock = mock[ExtensibleEffects.type]
-    assert(true)
-  }
-
-}
+class ExtensibleEffectsTest extends AnyFreeSpec {}
