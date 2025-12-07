d22e5e61e87fdc59eb5ef29d31495299bcdf7290
okoeee <yk27.puxcm@gmail.com>
2025-12-07 09:38:35 +0900

    implement chain of responsibility

---
+++ src/main/scala/designpattern/ChainOfResponsibility.scala
@@ -0,0 +1,29 @@
+package designpattern
+
+import cats.implicits._
+
+object ChainOfResponsibility {
+
+  /*
+   要求を処理するオブジェクトを1本のチェーン状に並べて順に処理を委譲していくデザインパターン
+   Webフレームワークのミドルウェア、バリデーション処理などで応用が可能
+   */
+
+  trait Handler {
+    def handle(req: String): Option[String]
+  }
+
+  class AuthHandler(next: Handler) extends Handler {
+    def handle(req: String): Option[String] =
+      if (req.contains("auth")) "Authorized".some
+      else next.handle(req)
+  }
+
+  class LogHandler(next: Handler) extends Handler {
+    def handle(req: String): Option[String] = {
+      println(s"log: $req")
+      next.handle(req)
+    }
+  }
+
+}
